12bb4ffa59c5774f9904998f3394a960b5ac236e
Ismaila Afeez <157530759+Harflexdev@users.noreply.github.com>
2024-08-05 13:55:31 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,11 +4,17 @@ AOS.init({
   once: false,
   mirror: false,
 });
- 
+
 const navLinks = document.getElementById("navLinks");
 const Menu = document.getElementById("menu");
 
 Menu.addEventListener("click", () => {
+  let isOpen = true;
+  if (isOpen == false) {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
   navLinks.classList.toggle("hidden");
   Menu.classList.toggle("active");
 });
